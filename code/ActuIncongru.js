45d750e9641fe216e5902c3527a6221d5fb3a66e
YoaN80490 <147514201+YoaN80490@users.noreply.github.com>
2023-12-15 15:59:56 +0100

    15/12/2023 16h

--- code/ActuIncongru.js
+++ code/ActuIncongru.js
@@ -5,3 +5,19 @@ icon.addEventListener("click", () => {
 croix.addEventListener("click", () => {
     popup.classList.toggle("fermer")
 })
+
+
+const change = (int) => {
+    if ($('#infosup'+int).is(':visible')) {
+        $('#infosup'+int).hide();
+        document.getElementById('arrow'+int).style.WebkitTransitionDuration='0.5s';
+        document.getElementById('arrow'+int).style.rotate ='0deg';
+    }
+    else {
+        $('#infosup'+int).show();
+        document.getElementById('arrow'+int).style.WebkitTransitionDuration='0.5s';
+        document.getElementById('arrow'+int).style.rotate ='180deg';
+        
+    }
+}
+
